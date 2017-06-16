0f2c607818301bae3baa8a5e8c6f091f7a442cd0
Luciano <lhitaliani@gmail.com>
2017-06-16 05:37:33 -0300

    se cambio diseño de las barras vida,mana,energia

--- Script/test/testInicioUI.cs
+++ Script/test/testInicioUI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class testInicioUI : MonoBehaviour {
 	
@@ -8,10 +9,10 @@ public class testInicioUI : MonoBehaviour {
         estaCanvas();
         cantidadCorrectaElementos();
         estaUIVida();
-        estaUIExp();
-        estaSimboloClase();
-        estaPanelLibroHabilidades();
+        estaUIMana();
+        estaUIEnergia();
         estaPanelHabilidades();
+        estaVentanaPersonaje();
 
         IntegrationTest.Pass();
 	}
@@ -42,84 +43,85 @@ public class testInicioUI : MonoBehaviour {
         }
     }
 
+    private void analisisColor(Color color, float r, float b, float g, string err)
+    {
+        Color c = new Color(r, g, b);
+        if (!c.Equals(color))
+        {
+            IntegrationTest.Fail();
+            Debug.Log(err);
+            Debug.Log("Se esperaba: " + color + " -> " + c);
+            Debug.Log(color.r);
+        }
+    }
+
+    private void analizar(int pos, string n,bool estado, string err)
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+        GameObject ui = canvas.transform.GetChild(pos).gameObject;
+
+        bool activo = ui.activeSelf;
+        string nombre = ui.name;
+
+        if (estado != activo || ui.name != n)
+        {
+            IntegrationTest.Fail();
+            Debug.Log(ui + " error: " + err);
+            Debug.Log("Activo: " + activo);
+            Debug.Log("Nombre: " + nombre);            
+        }
+
+        if (n.Contains("vida") || n.Contains("mana") || n.Contains("energia"))
+        {
+            GameObject hijo = ui.transform.GetChild(0).gameObject;
+
+            if (n.Contains("vida"))
+            {
+                float r = 1;
+                float b = 0;
+                float g = 0;
+                analisisColor(hijo.GetComponent<Image>().color, r, b, g, "El color de la vida no es correcto.");
+            }
+            else if (n.Contains("mana"))
+            {
+                float r = 0;
+                float b = 1;
+                float g = 0;
+                analisisColor(hijo.GetComponent<Image>().color, r, b, g, "El color del mana no es correcto.");
+            }
+            else
+            {
+                float r = 0;
+                float b = 0;
+                float g = 1;
+                analisisColor(hijo.GetComponent<Image>().color, r, b, g, "El color de la energia no es correcto.");
+            }
+        }
+    }
+
     private void estaUIVida()
     {
-        GameObject canvas = GameObject.Find("Canvas");
-        GameObject ui = canvas.transform.GetChild(0).gameObject;
-
-        bool activo = ui.activeSelf;
-        string nombre = ui.name;
-
-        if (!activo || ui.name != "ui_vida")
-        {
-            Debug.Log("Activo: " + activo);
-            Debug.Log("Nombre: " + nombre);
-            IntegrationTest.Fail();
-        }
+        analizar(0, "ui_vida", true, "UIVida");
     }
 
-    private void estaUIExp()
+    private void estaUIMana()
     {
-        GameObject canvas = GameObject.Find("Canvas");
-        GameObject ui = canvas.transform.GetChild(1).gameObject;
-
-        bool activo = ui.activeSelf;
-        string nombre = ui.name;
-
-        if (!activo || ui.name != "ui_exp")
-        {
-            Debug.Log("Activo: " + activo);
-            Debug.Log("Nombre: " + nombre);
-            IntegrationTest.Fail();
-        }
+        analizar(1, "ui_mana", true, "UIMana");
     }
 
-    private void estaSimboloClase()
+    private void estaUIEnergia()
     {
-        GameObject canvas = GameObject.Find("Canvas");
-        GameObject ui = canvas.transform.GetChild(2).gameObject;
-
-        bool activo = ui.activeSelf;
-        string nombre = ui.name;
-
-        if (!activo || ui.name != "ui_simbolo_clase")
-        {
-            Debug.Log("Activo: " + activo);
-            Debug.Log("Nombre: " + nombre);
-            IntegrationTest.Fail();
-        }
-    }
+        analizar(2, "ui_energia", true, "UIEnergia");
+    }    
 
     private void estaPanelHabilidades()
     {
-        GameObject canvas = GameObject.Find("Canvas");
-        GameObject ui = canvas.transform.GetChild(3).gameObject;
-
-        bool activo = ui.activeSelf;
-        string nombre = ui.name;
-
-        if (!activo || ui.name != "ui_panel_habilidades")
-        {
-            Debug.Log("Activo: " + activo);
-            Debug.Log("Nombre: " + nombre);
-            IntegrationTest.Fail();
-        }
+        analizar(3, "ui_panel_habilidades", true, "Panel Habilidades");
     }
 
-    private void estaPanelLibroHabilidades()
+    private void estaVentanaPersonaje()
     {
-        GameObject canvas = GameObject.Find("Canvas");
-        GameObject ui = canvas.transform.GetChild(4).gameObject;
-
-        bool activo = ui.activeSelf;
-        string nombre = ui.name;
-
-        if (activo || ui.name != "ui_libro_habilidades")
-        {
-            Debug.Log("Activo: " + activo);
-            Debug.Log("Nombre: " + nombre);
-            IntegrationTest.Fail();
-        }
+        analizar(4, "ui_ventana_personaje", false, "Ventana personaje");
     }
 	
 }
